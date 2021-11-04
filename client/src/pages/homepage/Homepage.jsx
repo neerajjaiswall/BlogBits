c28d320412d8e702f6fdee8c284f6711448358db
neerajjaiswall <neerajjaiswal8952@gmail.com>
2021-11-04 06:07:06 +0530

    Initial Commit

---
+++ client/src/pages/homepage/Homepage.jsx
@@ -0,0 +1,31 @@
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./homepage.css";
+import axios from "axios";
+
+export default function Homepage() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  // console.log(location);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      // console.log(res)
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
